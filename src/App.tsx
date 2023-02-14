943421f90b27f6743a84c4c49751bf454c6ee429
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-14 15:43:33 +0530

    feat: done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import { MouseEvent, useState } from "react"
+import { ImageType, IMAGE_DATA } from "./data"
+
 function App() {
+  const [image, setImage] = useState<ImageType | undefined>()
+
+  const handleOnClick = (image: ImageType) => {
+    setImage(image)
+  }
+
+  const handleClose = () => {
+    setImage(undefined)
+  }
+
+  const handleStopClose = (
+    e: MouseEvent<HTMLImageElement, globalThis.MouseEvent>
+  ) => {
+    e.stopPropagation()
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
       <h1>13/27 - Image Gallery</h1>
+      <div className="container">
+        <div className="gallery">
+          {IMAGE_DATA.map((image) => (
+            <img
+              className="image"
+              onClick={() => handleOnClick(image)}
+              key={image.id}
+              src={image.src}
+              alt={image.alt}
+            />
+          ))}
+        </div>
+      </div>
+      {image && (
+        <div onClick={handleClose} className="slides_container">
+          <div onClick={handleClose} className="close">
+            x
+          </div>
+          <img
+            onClick={handleStopClose}
+            className="slide"
+            src={image.src}
+            alt={image.alt}
+          />
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/data.ts
@@ -0,0 +1,85 @@
+export const IMAGE_DATA = [
+  {
+    id: 1,
+    src: "1.jpg",
+    alt: "car and ferris wheel in dark and fog",
+  },
+  {
+    id: 2,
+    src: "2.jpg",
+    alt: "snow covered mountain during daytime",
+  },
+  {
+    id: 3,
+    src: "3.jpg",
+    alt: "a view of a town with windmills lit up at night",
+  },
+  {
+    id: 4,
+    src: "4.jpg",
+    alt: "a large bridge over a large body of water",
+  },
+  {
+    id: 5,
+    src: "5.jpg",
+    alt: "city at sunset with reflection on river",
+  },
+  {
+    id: 6,
+    src: "6.jpg",
+    alt: "a body of water surrounded by a forest",
+  },
+  {
+    id: 7,
+    src: "7.jpg",
+    alt: "a snowy landscape with a church on top of a hill",
+  },
+  {
+    id: 8,
+    src: "8.jpg",
+    alt: "a mountain range with a red sky in the background",
+  },
+  {
+    id: 9,
+    src: "9.jpg",
+    alt: "a snow covered mountain with trees in the foreground",
+  },
+  {
+    id: 10,
+    src: "10.jpg",
+    alt: "two trees are silhouetted against the night sky",
+  },
+  {
+    id: 11,
+    src: "11.jpg",
+    alt: "mountains and trees in autumn",
+  },
+  {
+    id: 12,
+    src: "12.jpg",
+    alt: "aerial view of ocean waves",
+  },
+  {
+    id: 13,
+    src: "13.jpg",
+    alt: "a mountain is reflected in the still water of a lake",
+  },
+  {
+    id: 14,
+    src: "14.jpg",
+    alt: "silhouette of mountains",
+  },
+  {
+    id: 15,
+    src: "15.jpg",
+    alt: "crescent moon",
+  },
+  {
+    id: 16,
+    src: "16.jpg",
+    alt: "a tree in a field with a purple sky in the background",
+  },
+]
+
+export type ImageDataType = typeof IMAGE_DATA
+export type ImageType = ImageDataType[number]
